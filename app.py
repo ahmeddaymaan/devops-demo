dfebe0e82556676a95e786cea150d8b0ee5f6bfc
Ahmed Ayman <ahmedayman.a55@gmail.com>
2026-09-06 15:34:57 +0300

    Add version endpoint

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def health():
     return "OK"
 
+@app.route("/version")
+def version():
+    return "Version 1.0"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
